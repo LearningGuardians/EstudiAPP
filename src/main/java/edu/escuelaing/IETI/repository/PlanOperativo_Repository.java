8f695574a1fec8d9969ad6ea99f3c2ef8995b8b2
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-09-15 11:14:00 -0500

    Refactorización Materias

--- src/main/java/edu/escuelaing/IETI/repository/PlanOperativo_Repository.java
+++
@@ -1,7 +0,0 @@
-package edu.escuelaing.IETI.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PlanOperativo_Repository extends MongoRepository{
-
-}
